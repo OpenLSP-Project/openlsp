6ef6dbdcfad4177d00d9321a85ecdea62ae8186b
Kayce tian <mohan_82@163.com>
2026-08-07 11:00:52 +0800

    Set OpenLSP Core public API exports

--- openlsp-core/openlsp_core/__init__.py
+++ openlsp-core/openlsp_core/__init__.py
@@ -1,1 +1,34 @@
+"""
+OpenLSP Core Package
 
+Public API exports.
+Version: v0.4-alpha
+"""
+
+
+from .message import create_message
+
+from .session import Session
+
+from .validator import (
+    validate_message,
+    validate_message_or_raise
+)
+
+
+from .exceptions import (
+    OpenLSPError,
+    InvalidMessageError,
+    InvalidSessionError
+)
+
+
+__all__ = [
+    "create_message",
+    "Session",
+    "validate_message",
+    "validate_message_or_raise",
+    "OpenLSPError",
+    "InvalidMessageError",
+    "InvalidSessionError",
+]
